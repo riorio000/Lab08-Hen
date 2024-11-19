3c866b68ad4830730ae06d68e5b22f1b503f3f90
c0lethecoder <95252605+c0lethecoder@users.noreply.github.com>
2024-11-18 20:28:16 -0800

    5

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = "login"
+    login_manager.login_view = "views.login"  
 
     from .models import User
 
